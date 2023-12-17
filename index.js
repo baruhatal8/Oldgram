6b7cea46f16e073c7a21bdc846c3f1feb6beabbd
Mokimo <thesimpleflow@gmail.com>
2023-12-17 18:54:29 +0100

    Version with JS

--- index.js
+++ index.js
@@ -1,33 +1,60 @@
 const posts = [
-    {
-        name: "Vincent van Gogh",
-        username: "vincey1853",
-        location: "Zundert, Netherlands",
-        avatar: "images/avatar-vangogh.jpg",
-        post: "images/post-vangogh.jpg",
-        comment: "just took a few mushrooms lol",
-        likes: 21
-    },
-    {
-        name: "Gustave Courbet",
-        username: "gus1819",
-        location: "Ornans, France",
-        avatar: "images/avatar-courbet.jpg",
-        post: "images/post-courbet.jpg",
-        comment: "i'm feelin a bit stressed tbh",
-        likes: 4
-    },
-        {
-        name: "Joseph Ducreux",
-        username: "jd1735",
-        location: "Paris, France",
-        avatar: "images/avatar-ducreux.jpg",
-        post: "images/post-ducreux.jpg",
-        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
-    }
-]
+  {
+    name: "Vincent van Gogh",
+    username: "vincey1853",
+    location: "Zundert, Netherlands",
+    avatar: "images/avatar-vangogh.jpg",
+    post: "images/post-vangogh.jpg",
+    comment: "just took a few mushrooms lol",
+    likes: 21,
+  },
+  {
+    name: "Gustave Courbet",
+    username: "gus1819",
+    location: "Ornans, France",
+    avatar: "images/avatar-courbet.jpg",
+    post: "images/post-courbet.jpg",
+    comment: "i'm feelin a bit stressed tbh",
+    likes: 4,
+  },
+  {
+    name: "Joseph Ducreux",
+    username: "jd1735", 
+    location: "Paris, France",
+    avatar: "images/avatar-ducreux.jpg",
+    post: "images/post-ducreux.jpg",
+    comment:
+      "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+    likes: 152,
+  },
+];
 
-array.forEach(element => {
-    
-});
+  
+  let container = document.querySelector("main");
+  
+  let list = ""; // Инициализируем переменную вне цикла
+  
+  posts.forEach((item) => {
+    list += `
+      <section class="user">
+        <section class="title">
+          <img src="${item.avatar}" alt="mini" class="mini">
+          <section class="info">
+            <h3 class="font nick">${item.name}</h3>
+            <h4 class="font point">${item.location}</h4>
+          </section>
+        </section>
+        <img src="${item.post}" alt="post" class="post">
+        <section class="social">
+          <img src="images/icon-heart.png" alt="like" class="icon like">
+          <img src="images/icon-comment.png" alt="comment" class="icon comment">
+          <img src="images/icon-dm.png" alt="share" class="icon share">
+        </section>
+        <h4 class="likes">${item.likes} Likes</h4>
+        <p class="subs"><span class="username">${item.username}</span> ${item.comment}</p>
+      </section>
+    `;
+  });
+  
+  container.innerHTML = list; // Добавляем весь HTML в контейнер после формирования списка
+  
